350b6d98f81d0f6c13bc4b2590f79c1182b892b5
Philipp Sehnert <psikon86@gmail.com>
2013-06-28 16:47:09 +0200

    working on taxon

--- scratch.R
+++ scratch.R
@@ -9,100 +9,38 @@ require(plyr)
 
 # connection herstellen
 con <- blastReportDBConnect("/home/psehnert/daten/SPICEIII/miseq/sample64/blast.test.db")
+# ort der taxon db
 taxon <- "/home/psehnert/daten/SPICEIII/miseq/scripts/metpipe/program/db/"
-
-getHit <- function(x,id) {
-  db_query(x,paste("SELECT * FROM hit where query_id=",id))
-}
-
-getHsp <- function(x,id) {
-  db_query(x,paste("SELECT * FROM hsp where query_id=",id))
-}
 
 # extract tables for overview
 hit <- db_query(con, "SELECT * from hit")
 hsp <- db_query(con, "SELECT * from hsp")
 
-# generate test data
-hit_test <- getHit(con,74)
-hsp_test <- getHsp(con,74)
+# testvar
+id <- 1233
+x <- con
+# spaeterer Funktionskoerper
+#lapply(hit$query_id,FUN=function(id) {
 
 # filter hits for query coverage
 cHits <- getHit(x,id)[which(getQueryCoverage(x,id) >=0.50),]
 
+#if(nrow(cHits)>=1){
 # filter Hsp(s) for query Coverage
-chsp <- getSelected(con, 74,getHitID(x,id)[which(getQueryCoverage(x,id) >=0.50)])
+chsp <- getSelected(con, id,getHitID(x,id)[which(getQueryCoverage(x,id) >=0.50)])
+chsp <- filterHsp(con,chsp,perc=0.98)
+cHits <- reduceHitsFromHsps(cHits,chsp)
 
-getSelected <- function (x, qid,hid) {
-  stmts <- paste("SELECT * from hsp WHERE query_id=",qid,"AND hit_id=",hid)
-  as.data.frame(do.call(rbind,lapply(stmts, FUN=function(stmt) {
-    db_query(x, stmt) %||% NA_character_ 
-  })))
-}
-
-# reduce the hsp to one if necessary
-chsp_range <- getSelectedRange(con,chsp)
-
-getSelectedRange <- function(x,df) {
-  stmts <- paste("SELECT hit_id, query_from, query_to, hit_from, hit_to FROM 
-                 hsp WHERE query_id=",unique(df$query_id),"AND hit_id=",unique(df$hit_id))
- lapply(stmts, FUN=function(stmt) {
-    pos <- db_query(x, stmt) %||% NA_character_
-    IRanges(start=pos$query_from,end=pos$query_to,names=pos$hit_id)
-  })
-}
-
-#TODO:
-# vereinige die selektierten HSp zu einem und ranke sie dann. anschkließend noch mal 
-# selektion mit 98% des Total Scores und dann begin der Zuordnung der TaxIds
-
-getQueryRange(x,id)
-reduce(chsp)
-# rank the hsps for bitscore
-chsp <- arrange(chsp,desc()
-)
-
-voteTaxID <- function(x,id) {
-  hits <- db_query(con,paste("SELECT * from hit WHERE query_id=",id))
-  taxa <- taxonByGeneID(hits[,'gene_id'],taxon)
-  taxa@.Data
-  
-  getTaxId(taxa)
-  vapply(taxa,getParentTaxId,character(1))
-  getParentTaxId(taxa)
-}
-
-hit_ids <- 
-
-cHits <- 
-
-
-                   
-
-getAlignLen(x,id)
-getHitID(x,id)[which(getQueryCoverage(x,id) >=0.50)]
-
-vapply
-cHsp <- apply(getHsp(x,id),FUN= subset()
-
-selectHits <- function(x,id) {
- 
- cHits <- getHit(x,id)
- cHsp <- getHsp(x,id)
- taxa <- taxonByGeneID(cHits$gene_id[1],taxon)
- taxa@ParentTaxId
- taxonDB(8139,taxon)
- select <- candidates[getHitCoverage(x,id),]
- candidates <- db_query(con,paste("select * from hit WHERE queryid=",id,AND hit_id ==))
-}
-
-
-
-
-
-
-
-
-
-
-
+taxa <- taxonByGeneID(cHits$gene_id, taxon)
+print(getRank(taxa))
+getLineage(taxa)
+getParentTaxId(taxa)
+taxonDB(getParentTaxId(taxa), taxon)
+#if (getRank(taxa)=='no rank') {
+#  print(id)
+#  break
+# }}}
+       )
+getTaxId(test)
+test
+test[[6]]
